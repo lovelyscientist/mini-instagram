66ec177c9fd2aaa19f3077aef8b454e4dda3a4f6
Vladyslava Tyshchenko <vtysch@softserveinc.com>
2016-05-31 17:57:31 +0300

    main logic implemented

--- js/controller.js
+++ js/controller.js
@@ -1,39 +1,40 @@
+instagramApp.controller('photoController', function photoController($scope, $http) {
+    var counter = 0,
+        names = [];
+        isNeedToLoad = false;
+        $scope.items = [];
 
+    init();
 
-instagramApp.directive('whenScrolled', function() {
-    return function(scope, elm, attr) {
-        var raw = elm[0];
-        elm.bind('scroll', function() {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-
-    };
-});
-
-
-instagramApp.controller('photoController', function photoController($scope) {
-    $scope.items = [];
-    $scope.isNeedToLoad = false;
-
-     var counter = 0;
-    for (var i = 0; i < 12; i++) {
-            $scope.items.push({id: counter});
-            counter += 10;
+    function init () {
+         $http({method: 'GET', url: '/firstImages'})
+          .then(function successCallback(response) {
+              names = response.data.src;
+              console.log($scope.names);
+              for (let i = 0; i < 12; i++) {
+                  $scope.items.push({id: counter, name: names[counter]});
+                  counter += 1;
+              }
+              counter = counter-12;
+          }, function errorCallback(response) {});
     }
-
+         
     $scope.startLoading = function () {
-        $scope.isNeedToLoad = true;
-        $scope.loadMore();
+        isNeedToLoad = true;
+        $http({method: 'GET', url: '/images'})
+        .then(function successCallback(response) {
+              names = response.data.src;
+              $scope.loadMore();
+        }, function errorCallback(response) {});
     }
 
     $scope.loadMore = function() {
-        if ($scope.isNeedToLoad) {
+        if (isNeedToLoad) {
           document.getElementsByTagName('button')[0].style.display = 'none';
-          for (var i = 0; i < 3; i++) {
-              $scope.items.push({id: counter});
-              counter += 10;
+         
+          for (let i = 0; i < 3; i++) {
+              $scope.items.push({id: counter, name: names[counter]});
+              counter++;
           }
         }
     };
---
+++ server/server.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var server = http.createServer();
+var imagesCount = 0;
+
+var fileExtentions = {
+    '.html':'text/html',
+    '.css':'text/css',
+    '.js':'text/javascript',
+    '.json':'application/json',
+    '.jpg': 'image'
+};
+
+server.on('request', function(req, res) { 
+  var extention = path.extname(req.url),
+      type = fileExtentions[extention], 
+      filePath =  '../' + req.url;
+
+  if (req.url.indexOf('.') > -1) {
+    fs.exists(filePath, function (exists) {
+        if (exists && req.url !== '/') {
+            fs.readFile(filePath, function (err, data){
+                res.writeHead(200, {'Content-Type': type});
+                console.log('Got request: ' + req.url);
+                res.write(data, function (err) {
+                   res.end();
+                });
+            });
+        } else {
+           res.writeHead(404);
+           res.write('Oops.. Page is not found!');
+           res.end();
+        }
+    });
+  }
+
+  if (req.url === '/images') {
+    getImages(__dirname + '/images', function (err, files) {
+       var names = {src: files.slice(12, files.length - 1)};
+           res.writeHead(200);
+           res.write(JSON.stringify(names));
+           res.end();
+    });
+  }
+
+  if (req.url === '/firstImages') {
+    getImages(__dirname + '/images', function (err, files) {
+       var names = {src: files.slice(0,12)};
+           res.writeHead(200);
+           res.write(JSON.stringify(names));
+           res.end();
+    });
+  }
+
+});
+  
+console.log('server started on localhost:3000');
+server.listen(3000);
+
+function getImages(p, callback) {
+    fs.readdir(p, function (err, files) {
+          if (err) {
+              throw err;
+          }
+          callback(err, files);
+    });
+}
